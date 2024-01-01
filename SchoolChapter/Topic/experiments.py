878d3939ee68e37166558fb759cfe3c3a61ff17c
Parteek Sharma <sparteek65@Parteeks-MacBook-Air.local>
2024-01-01 18:38:42 +0530

    added experiments

---
+++ SchoolChapter/Topic/experiments.py
@@ -0,0 +1,11 @@
+from django.http.response import JsonResponse
+from django.shortcuts import redirect
+from django.shortcuts import render
+
+def redirect_to_url(request):
+    os = request.GET.get("os")
+    code = request.GET.get("code")
+    deeplink = f'unitydl:mylink?code={code}'
+    
+    # Redirect to the Unity3D link
+    return render(request,'experiment.html',context={"deeplink":deeplink})
